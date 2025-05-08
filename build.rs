bf31934676187711c326b178190eb66f4cae2f4c
PTFOPlayer <patryk041103@gmail.com>
2025-05-08 20:44:19 +0200

    macro for creating hip kernels

--- build.rs
+++ build.rs
@@ -1,5 +1,28 @@
+use std::{fs, path::Path, process::Command};
 
 fn main() {
-    println!("cargo:rustc-link-lib=amdhip64");
-}
+    let kernel_dir = Path::new("kernel_sources");
 
+    if !kernel_dir.exists() {
+        return;
+    }
+
+    for entry in fs::read_dir(kernel_dir).unwrap() {
+        let Ok(entry) = entry else {
+            continue;
+        };
+
+        let entry = entry.path();
+        let entry = entry.to_str().unwrap();
+        let entry = "./".to_owned() + entry;
+
+        let status = Command::new("sh")
+            .arg("-c")
+            .arg("cd ./kernel_sources/kernel/; cargo build --release; cd ./../../;")
+            .status()
+            .expect("Failed to execute cargo build");
+        if !status.success() {
+            panic!("Kernel compilation failed for {:?}", entry);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,64 @@
-use std::ffi::CString;
+use std::path::PathBuf;
 
-use hip_runtime_sys::*;
+
+use hip_poc_macros::amdgpu_kernel;
+use rocm_rs::hip::*;
+
 const LEN: usize = 1024;
-fn main() {
-    unsafe {
-        let result = hipSetDevice(0);
-        assert_eq!(result, hipError_t::hipSuccess);
 
-        let mut input: hipDeviceptr_t = std::ptr::null_mut();
-        let mut output: hipDeviceptr_t = std::ptr::null_mut();
-        let result = hipMalloc(&mut input, LEN);
-        assert_eq!(result, hipError_t::hipSuccess);
-        let result = hipMalloc(&mut output, LEN);
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        const LEN: usize = 1024;
-        let mut in_host: Vec<u8> = vec![0; LEN];
-        let mut out_host: Vec<u8> = vec![0; LEN];
-
-        for i in 0..LEN {
-            in_host[i] = (i % 10) as u8;
+amdgpu_kernel! {
+    #[unsafe(no_mangle)]
+    pub extern "gpu-kernel" fn kernel(input: *mut u8, output: *mut u8) {
+        let id = unsafe { workitem_id_x() as usize };
+        unsafe {
+            *output.add(id) = *input.add(id) * 2;
         }
-
-        let result = hipMemcpyHtoD(input, in_host.as_mut_ptr() as *mut std::ffi::c_void, LEN);
-        assert_eq!(result, hipError_t::hipSuccess);
-        let module_data =
-            std::fs::read("kernels/target/amdgcn-amd-amdhsa/release/kernels.elf").unwrap();
-        let mut module: hipModule_t = std::ptr::null_mut();
-        let result =
-            hipModuleLoadData(&mut module, module_data.as_ptr() as *const std::ffi::c_void);
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        let mut function: hipFunction_t = std::ptr::null_mut();
-        let kernel_name = CString::new("kernel").expect("Invalid kernel name");
-        let result = hipModuleGetFunction(&mut function, module, kernel_name.as_ptr());
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        // Assemble arguments for the kernel.
-        // Pass two pointers,
-        let kernel_args: &mut [*mut std::ffi::c_void] = &mut [
-            input as *mut std::ffi::c_void,
-            output as *mut std::ffi::c_void,
-        ];
-        let mut size = std::mem::size_of_val(kernel_args);
-
-        let mut config = [
-            0x1 as *mut std::ffi::c_void,                   // Next come arguments
-            kernel_args as *mut _ as *mut std::ffi::c_void, // Pointer to arguments
-            0x2 as *mut std::ffi::c_void,                   // Next comes size
-            std::ptr::addr_of_mut!(size) as *mut std::ffi::c_void, // Pointer to size of arguments
-            0x3 as *mut std::ffi::c_void,                   // End
-        ];
-        let result = hipModuleLaunchKernel(
-            function,
-            2,                    // Workgroup count x
-            1,                    // Workgroup count y
-            1,                    // Workgroup count z
-            LEN as u32 / 2,       // Workgroup dim x
-            1,                    // Workgroup dim y
-            1,                    // Workgroup dim z
-            LEN as u32 / 2,       // sharedMemBytes for extern shared variables
-            std::ptr::null_mut(), // stream
-            std::ptr::null_mut(), // params (unimplemented in hip)
-            config.as_mut_ptr(),  // arguments
-        );
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        let result = hipDeviceSynchronize();
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        let result = hipMemcpyDtoH(out_host.as_mut_ptr() as *mut std::ffi::c_void, output, LEN);
-        assert_eq!(result, hipError_t::hipSuccess);
-
-        println!("Output: {:?}", &out_host[..32]);
     }
+}
+
+fn main() -> Result<()> {
+    let device = Device::new(0)?;
+    device.set_current()?;
+
+    let kernel_path = PathBuf::from("./kernel_sources/kernel/target/amdgcn-amd-amdhsa/release/kernels.elf");
+    assert!(kernel_path.exists());
+
+    let module = Module::load(kernel_path)?;
+
+    let function = unsafe { module.get_function("kernel")? };
+
+    let mut in_host: Vec<u8> = vec![0; LEN];
+    let mut out_host: Vec<u8> = vec![0; LEN];
+
+    for i in 0..LEN {
+        in_host[i] = (i % 10) as u8;
+    }
+
+
+    let mut input = DeviceMemory::<u8>::new(LEN)?;
+    let output = DeviceMemory::<u8>::new(LEN)?;
+
+    input.copy_from_host(&in_host)?;
+
+    let kernel_args = [
+        input.as_kernel_arg(),
+        output.as_kernel_arg(),
+    ];
+
+    let grid_dim = Dim3 { x: 2, y: 1, z: 1 };
+    let block_dim = Dim3 { x: (LEN/ 2) as u32, y: 1, z: 1 };
+
+    function.launch(
+        grid_dim,
+        block_dim,
+        0, 
+        None,
+        &mut kernel_args.clone(),
+    )?;
+
+    output.copy_to_host(&mut out_host)?;
+
+    println!("Output: {:?}", &out_host[..32]);
+
+    Ok(())
 }
